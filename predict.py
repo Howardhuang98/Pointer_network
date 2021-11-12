5265c3f76f7dad2132433dd42ba902c7f3738a79
HUANG HAO <934303306@qq.com>
2021-11-12 19:09:38 +0800

    增加predict

--- predict.py
+++ predict.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 """
-@File    :   predict.py
+@File    :   predict.py    
 @Contact :   huanghoward@foxmail.com
-@Modify Time :    2021/11/11 9:38  
+@Modify Time :    2021/11/12 16:58  
 ------------      
 """
-
 import numpy as np
 from keras.layers import Input
 from keras.models import Model
-from keras.utils.np_utils import to_categorical
 
-from utils import Tsp
 from model import *
+from utils import cost
 
-# 生成问题
-p = Tsp()
-X, Y = p.next_batch(2)
-YY = []
-for y in Y:
-    YY.append(to_categorical(y))
-YY = np.asarray(YY)
-print(X,YY)
+x_test = np.load(r"tsp_data/tsp_5/tsp5_test_x.npy")
+y_test = np.load(r"tsp_data/tsp_5/tsp5_test_y.npy")
+
 # 构建模型
-main_input = Input(shape=(X.shape[1], 2), name='main_input')
+main_input = Input(shape=(x_test.shape[1], 2), name='main_input')
 enc_output, state_h, state_c = Encoder()(main_input)
 outputs = Decoder()(enc_output, [state_h, state_c])
 model = Model(main_input, outputs)
 print(model.summary())
+# 指定训练配置
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 # 加载权重
-model.load_weights(r"data/tmp/checkpoint-2021-11-11-20-56-45")
-y = model.predict(X)
-y = y.argmax(axis=2)
-p.cost(X,y)
-print(p.cost(X,y),p.cost(X,Y))
+model.load_weights(r"./data/ckp-2021-11-12-11-45-37/checkpoint")
+
+y = model.predict(x_test)
+model_cost = cost(x_test,y)
+optimal_cost = cost(x_test,y_test)
+
+print("模型预测路径长度-最短路径=",sum(model_cost-optimal_cost)/x_test.shape[0])
+
--- utils.py
+++ utils.py
@@ -14,87 +14,38 @@ import numpy as np
 from keras.utils.np_utils import to_categorical
 from tqdm import tqdm
 
-
-class Tsp:
+def cost(x,y):
     """
-    简单的Tsp问题类，用于生成数据
-    如果需要大量的数据请查看tsp_data，由Pointer network 作者提供
-    http://goo.gl/NDcOIG
+    用于计算
+    :param x: shape = [batch, n, 2]
+    :param y: shape = [batch, n, n]
+    :return:
     """
-    def next_batch(self, batch_size=1):
-        X, Y = [], []
-        for b in tqdm(range(batch_size)):
-            points = self.generate_data()
-            solved = self.solve_tsp_dynamic(points)
-            X.append(points), Y.append(solved)
-        return np.asarray(X), np.asarray(Y)
+    if x.shape[0] != y.shape[0]:
+        raise ValueError("Error!")
+    else:
+        batch_size = x.shape[0]
+        travels = []
+        for i in range(batch_size):
+            points = x[i]
+            idx = y[i].argmax(axis=1)
+            travel = 0
+            for j in range(len(idx)-1):
+                city1 = idx[j]
+                city2 = idx[j+1]
+                travel = np.sqrt(sum(np.power(points[city2]-points[city1],2)))
+                travel += travel
+            travels.append(travel)
+        travels = np.asarray(travels)
+        return travels
 
-    def length(self, x, y):
-        return (math.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2))
 
-    def solve_tsp_dynamic(self, points):
-        # calc all lengths
-        all_distances = [[self.length(x, y) for y in points] for x in points]
-        # initial value - just distance from 0 to
-        # every other point + keep the track of edges
-        A = {(frozenset([0, idx + 1]), idx + 1): (dist, [0, idx + 1])
-             for idx, dist in enumerate(all_distances[0][1:])}
-        cnt = len(points)
-        for m in range(2, cnt):
-            B = {}
-            for S in [frozenset(C) | {0}
-                      for C in itertools.combinations(range(1, cnt), m)]:
-                for j in S - {0}:
-                    B[(S, j)] = min([(A[(S - {j}, k)][0] + all_distances[k][j],
-                                      A[(S - {j}, k)][1] + [j])
-                                     for k in S if k != 0 and k != j])
-            A = B
-        res = min([(A[d][0] + all_distances[0][d[1]], A[d][1])
-                   for d in iter(A)])
-        return res[1]
 
-    def generate_data(self, N=10):
-        radius = 1
-        rangeX = (0, 10)
-        rangeY = (0, 10)
-        qty = N
 
-        deltas = set()
-        for x in range(-radius, radius + 1):
-            for y in range(-radius, radius + 1):
-                if x * x + y * y <= radius * radius:
-                    deltas.add((x, y))
 
-        randPoints = []
-        excluded = set()
-        i = 0
-        while i < qty:
-            x = random.randrange(*rangeX)
-            y = random.randrange(*rangeY)
-            if (x, y) in excluded:
-                continue
-            randPoints.append((x, y))
-            i += 1
-            excluded.update((x + dx, y + dy) for (dx, dy) in deltas)
-        return randPoints
-
-    def cost(self, x, y):
-        cost = 0
-        for i in range(len(y[0]) - 1):
-            point_idx = y[0][i]
-            point_idx2 = y[0][i + 1]
-            cost += self.length(x[0][point_idx], x[0][point_idx2])
-        return cost
 
 
 if __name__ == "__main__":
-    # 准备数据
-    p = Tsp()
-    X, Y = p.next_batch(1000000)
-    YY = []
-    for y in Y:
-        YY.append(to_categorical(y))
-    YY = np.asarray(YY)
-    np.save(r"data/X-1000000.npy", X)
-    np.save(r"data/Y-1000000.npy", Y)
-    np.save(r"data/YY-1000000.npy", YY)
+    x_test = np.load(r"tsp_data/tsp_5/tsp5_test_x.npy")
+    y_test = np.load(r"tsp_data/tsp_5/tsp5_test_y.npy")
+    print(cost(x_test,y_test))
